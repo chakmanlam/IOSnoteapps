3d81df92dfbeb7aff9fcea941541cae4ce956f11
Chakman Lam <chakman@upgrowth.one>
2025-05-22 10:29:18 -0400

    Initial Commit

---
+++ AINotes/AINotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  AINotesApp.swift
+//  AINotes
+//
+//  Created by Chak Man Lam on 5/22/25.
+//
+
+import SwiftUI
+
+@main
+struct AINotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AINotesTests/AINotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  AINotesTests.swift
+//  AINotesTests
+//
+//  Created by Chak Man Lam on 5/22/25.
+//
+
+import Testing
+@testable import AINotes
+
+struct AINotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
